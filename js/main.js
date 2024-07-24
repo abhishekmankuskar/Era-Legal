6d6845e9e765ce536843c44a3139229a2cd07580
Abhishek Mankuskar <mankuskarabhi2@gmail.com>
2024-07-24 10:45:01 +0530

    era leagel first update

---
+++ js/main.js
@@ -0,0 +1,148 @@
+(function ($) {
+  "use strict";
+
+  // Dropdown on mouse hover
+  $(document).ready(function () {
+    function toggleNavbarMethod() {
+      if ($(window).width() > 992) {
+        $(".navbar .dropdown")
+          .on("mouseover", function () {
+            $(".dropdown-toggle", this).trigger("click");
+          })
+          .on("mouseout", function () {
+            $(".dropdown-toggle", this).trigger("click").blur();
+          });
+      } else {
+        $(".navbar .dropdown").off("mouseover").off("mouseout");
+      }
+    }
+    toggleNavbarMethod();
+    $(window).resize(toggleNavbarMethod);
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
+
+  // Date and time picker
+  $("#date").datetimepicker({
+    format: "L",
+  });
+  $("#time").datetimepicker({
+    format: "LT",
+  });
+
+  // Service carousel
+  $(".service-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    margin: 30,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+      '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
+
+  // Team carousel
+  $(".team-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    margin: 30,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+      '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      992: {
+        items: 4,
+      },
+    },
+  });
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    center: true,
+    autoplay: true,
+    smartSpeed: 1000,
+    margin: 30,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
+})(jQuery);
+
+// JavaScript for Popup Modal
+document.addEventListener("DOMContentLoaded", function () {
+  const popupModal = document.getElementById("popupModal");
+  const acceptButton = document.getElementById("acceptButton");
+
+  // Check if the user has accepted within the last 12 hours
+  const acceptanceTimestamp = localStorage.getItem("popupAcceptedTimestamp");
+  const twelveHours = 12 * 60 * 60 * 1000; // 12 hours in milliseconds
+  const currentTime = new Date().getTime();
+
+  if (!acceptanceTimestamp || currentTime - acceptanceTimestamp > twelveHours) {
+    // Show the popup modal
+    popupModal.style.display = "flex"; // Changed from "block" to "flex"
+  }
+
+  // Handle the Accept button click
+  acceptButton.addEventListener("click", function () {
+    // Store the acceptance timestamp in local storage
+    localStorage.setItem("popupAcceptedTimestamp", new Date().getTime());
+
+    // Hide the popup modal
+    popupModal.style.display = "none";
+  });
+});
